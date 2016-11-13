f70a2bb85a9125b49a498f1960d73025a50186ce
Jinsu Mathew <jinsu@askme.in>
2016-11-12 22:44:33 -0600

    Modularised the translator constructor and functions

--- app.js
+++ app.js
@@ -1,117 +1,22 @@
 /**
- * Credit Unit Object
+ * Intergalactic transaction translator app
  */
-function CreditUnit(decimal, romain) {
-  this.decimal = decimal || null;
-  this.romain = romain || null;
-  this.symbol = null;
-}
 
-/**
- * Credit Object as a reference array
- */
-function Credit() {
-  this.reference = [];
-  this.iron = 1;
-  this.silver = 1;
-  this.gold = 1;
-  this.init();
-}
+// Module dependencies
+var Translator = require('./translator');
 
-/**
- * Initializes credit system with Romain letters and returns it as a reference array
- * @return {array} creditDict
- */
-Credit.prototype.init = function () {
-  var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  var romain = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+var translator = new Translator();
+translator.setSymbol('glob', 'I');
+translator.setSymbol('prok', 'V');
+translator.setSymbol('pish', 'X');
+translator.setSymbol('tegj', 'L');
 
-  for (var i = 0; i < decimal.length; i++) {
-    this.reference.push(new CreditUnit(decimal[i], romain[i]));
-  }
-};
-
-/**
- * Sets a symbol as a reference to a romain letter
- * @param {string} romain
- * @param {string} symbol
- * @return {integer} -1 or 0, if not set or set
- */
-Credit.prototype.setSymbol = function (romain, symbol) {
-  var creditDictIndex = this.reference.findIndex(o => o.romain === romain);
-  if (creditDictIndex === -1)
-    return -1;
-  this.reference[creditDictIndex].symbol = symbol;
-  return 0;
-};
-
-/**
- * Set metal prices based on the credits calculate from assoicated symbols
- * @param {string} metal, can be Iron, Silver or Gold
- * @param {integer} symbolsCredits
- * @param {integer} totalCredits
- * @return {integer} value, the set value or -1 if not set
- */
-Credit.prototype.setMetalPrice = function (metal, symbolsCredits, totalCredits) {
-  var metalName = metal.toLowerCase();
-
-  // check whether it's a valid metal
-  if (!['iron', 'silver', 'gold'].includes(metalName)) {
-    return -1;
-  }
-
-  this[metalName] = totalCredits / symbolsCredits;
-  return this[metalName];
-};
-
-/**
- * Calculate credit based on given symbol combinations
- * @param {array} symbols
- * @param {integer} credits
- */
-Credit.prototype.calculate = function (symbols) {
-  var credits = 0;
-  var romain = '';
-  var metalName = null;
-
-  // if the last symbol is a metal
-  if (['iron', 'silver', 'gold'].includes(symbols[symbols.length - 1].toLowerCase())) {
-    metalName = symbols.pop().toLowerCase();
-  }
-
-  // Construct the romain format
-  for (var i = 0; i < symbols.length; i++) {
-    var creditUnit = this.reference.find(o => o.symbol === symbols[i]);
-    if (creditUnit) {
-      romain += creditUnit.romain;
-    }
-  }
-
-  // Convert romain to decimal numbers
-  for (i = 0; i < this.reference.length; i++) {
-    while (romain.indexOf(this.reference[i].romain) === 0) {
-      credits += this.reference[i].decimal;
-      romain = romain.replace(this.reference[i].romain, '');
-    }
-  }
-
-  // if metal conbination
-  if (metalName) {
-    credits = credits * this[metalName];
-  }
-
-  return credits;
-};
-
-var credit = new Credit();
-credit.setSymbol('I', 'glob');
-credit.setSymbol('V', 'prok');
-credit.setSymbol('X', 'pish');
-credit.setSymbol('L', 'tegj');
-
+// Test cases
 var str = 'pish tegj glob glob';
-console.log(credit.calculate(str.split(' ')));
+console.log(translator.calculate(str.split(' ')));
+
 var symbolMetal = 'glob glob';
-console.log(credit.setMetalPrice('Silver', credit.calculate(symbolMetal.split(' ')), 34));
+console.log(translator.setMetalPrice('Silver', translator.calculate(symbolMetal.split(' ')), 34));
+
 var metalCombo = 'glob prok Silver';
-console.log(credit.calculate(metalCombo.split(' ')));
+console.log(translator.calculate(metalCombo.split(' ')));
---
+++ translator.js
@@ -0,0 +1,121 @@
+/**
+ * Constants as representation of decimal to romain letter mapping
+ */
+const DECIMAL = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+const ROMAIN = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+/**
+ * Intergalactic representation of credit units
+ */
+function IntergalacticUnit(decimal, romain) {
+  this.decimal = decimal || null;
+  this.romain = romain || null;
+
+  // an Intergalactic unit can be mapped to special
+  // objects like 'glob', 'prok', 'pish', 'tegj', etc. */
+  this.symbol = null;
+}
+
+/**
+ * Translator reference object
+ */
+function Translator() {
+  this.dict = [];
+
+  // Special metals
+  this.iron = 1;
+  this.silver = 1;
+  this.gold = 1;
+
+  // builds the Translator dict on construct call
+  this.init();
+}
+
+/**
+ * Initializes the Translator dict
+ * @method init
+ */
+Translator.prototype.init = function () {
+  for (var i = 0; i < DECIMAL.length; i++) {
+    this.dict.push(new IntergalacticUnit(DECIMAL[i], ROMAIN[i]));
+  }
+};
+
+/**
+ * Sets a symbol as reference to a romain letter/IntergalacticUnit
+ * @method setSymbol
+ * @param {string} symbol - things like 'glob', 'prok', 'pish', 'tegj', etc.
+ * @param {string} romain
+ * @return {IntergalacticUnit} intergalacticObject or -1 if not set
+ */
+Translator.prototype.setSymbol = function (symbol, romain) {
+  var dictIndex = this.dict.findIndex(o => o.romain === romain);
+  if (dictIndex === -1)
+    return -1;
+  this.dict[dictIndex].symbol = symbol;
+  return this.dict[dictIndex];
+};
+
+/**
+ * Set metal prices based on the credits calculate from assoicated symbols
+ * @method setMetalPrice
+ * @param {string} metal - can be Iron, Silver or Gold
+ * @param {integer} creditFromSymbols - calculated value by adding symbols
+ * @param {integer} totalCredits
+ * @return {integer} metalPrice or -1 if not set
+ */
+Translator.prototype.setMetalPrice = function (metal, creditFromSymbols, totalCredits) {
+  var metalName = metal.toLowerCase();
+
+  // check whether it's a valid metal
+  if (!['iron', 'silver', 'gold'].includes(metalName)) {
+    return -1;
+  }
+
+  this[metalName] = totalCredits / creditFromSymbols;
+  return this[metalName];
+};
+
+/**
+ * Calculate credit based on given symbol combinations
+ * @method calculate
+ * @param {array} symbols - can be ['glob', 'pish', 'silver']
+ * @return {integer} credits
+ * @todo return -1 if invalid symbols exists in the input array
+ */
+Translator.prototype.calculate = function (symbols) {
+  var credits = 0;
+  var romain = '';
+  var metalName = null;
+  var intergalacticUnit = null;
+
+  // if the last symbol is a metal
+  if (['iron', 'silver', 'gold'].includes(symbols[symbols.length - 1].toLowerCase())) {
+    metalName = symbols.pop().toLowerCase();
+  }
+
+  // Construct the romain format
+  for (var i = 0; i < symbols.length; i++) {
+    intergalacticUnit = this.dict.find(o => o.symbol === symbols[i]);
+    if (intergalacticUnit) {
+      romain += intergalacticUnit.romain;
+    }
+  }
+
+  // Convert romain to decimal numbers
+  for (i = 0; i < this.dict.length; i++) {
+    while (romain.indexOf(this.dict[i].romain) === 0) {
+      credits += this.dict[i].decimal;
+      romain = romain.replace(this.dict[i].romain, '');
+    }
+  }
+
+  // if metal conbination
+  if (metalName) {
+    credits = credits * this[metalName];
+  }
+
+  return credits;
+};
+
+module.exports = Translator;
